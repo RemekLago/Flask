d54387d48af111315cb254f1f68b47dedb73b5b1
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-02-15 23:17:07 +0100

    update

--- Packages/app.py
+++ Packages/app.py
@@ -1,12 +1,19 @@
+from crypt import methods
 from flask import Flask, render_template, request
 import packages
 
 app = Flask(__name__)
-debug = True
+#app.config["DEBUG"] = True
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def main():
-    return render_template("index.html")
+    list1 = {"ala": 1, "kot": 1, "tata": 1}
+    list2 = {"ala": 3, "kot": 3, "tata": 3}
+    list3 = {"ala": 7, "kot": 7, "tata": 7}
+    main_list = [list1, list2, list3]
+    return render_template("index.html", context=main_list)
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
